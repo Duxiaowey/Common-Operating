2493a9fca7faa5ca89497d1817b30cd7a0a1e067
Duxiaowey <44520025+Duxiaowey@users.noreply.github.com>
2019-12-30 14:22:54 +0800

    划分数据集，并写入ImageSets/Main

--- split_dataset.py
+++ split_dataset.py
@@ -1,7 +1,6 @@
 import os
 import random
 
-
 def split_trainval():
     trainval_percent = 0.8
     train_percent = 0.9
@@ -15,10 +14,10 @@ def split_trainval():
     trainval = random.sample(list, tv)
     train = random.sample(trainval, tr)
 
-    ftrainval = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImagesSets/Main/trainval.txt', 'w')
-    ftest = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImagesSets/Main/test.txt', 'w')
-    ftrain = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImagesSets/Main/train.txt', 'w')
-    fval = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImagesSets/Main/val.txt', 'w')
+    ftrainval = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImageSets/Main/trainval.txt', 'w')
+    ftest = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImageSets/Main/test.txt', 'w')
+    ftrain = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImageSets/Main/train.txt', 'w')
+    fval = open('/home/duxiaowey/my_ps/src/faster-rcnn/data/CASIA/CASIA1/ImageSets/Main/val.txt', 'w')
 
     for i in list:
         name = total_xml[i][:-4] + '\n'
@@ -35,7 +34,6 @@ def split_trainval():
     ftrain.close()
     fval.close()
     ftest.close()
-
 
 if __name__ == '__main__':
     split_trainval()
